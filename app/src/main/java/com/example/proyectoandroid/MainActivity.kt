4b59eb3274bf63dc24fcb61c7dc923b9bdd56392
antoniohdz117 <antonio117.berrios@gmail.com>
2025-05-20 14:12:45 -0600

    Proyecto Android

--- app/src/main/java/com/example/proyectoandroid/MainActivity.kt
+++ app/src/main/java/com/example/proyectoandroid/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : ComponentActivity() {
             ProyectoAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
+
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
