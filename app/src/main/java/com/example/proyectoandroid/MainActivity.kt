da79fb9e56a93a19a3c468fb2ed50ce3d6e44591
antoniohdz117 <antonio117.berrios@gmail.com>
2025-06-01 09:06:14 -0600

    splash de carga

--- app/src/main/java/com/example/proyectoandroid/MainActivity.kt
+++ app/src/main/java/com/example/proyectoandroid/MainActivity.kt
@@ -15,16 +15,7 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
 
 
-
-
-        val btnContraReloj: Button = findViewById(R.id.contraReloj)
-        val btnInfinito: Button = findViewById(R.id.infinito)
-        val btnDesafio: Button = findViewById(R.id.desafio)
-        val btnJugar: Button = findViewById(R.id.jugar)
-        val btnSonido: ImageButton = findViewById(R.id.volumen)
-        val btnConfiguracion: ImageButton = findViewById(R.id.configuracion)
-
-        btnJugar.setOnclickListener
+        //
 
 
 
